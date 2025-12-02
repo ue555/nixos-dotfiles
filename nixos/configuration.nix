0385e49c67ca83c862502829566f0ee81d328862
UKouji555 <zerozero53205320.dev@gmail.com>
2025-12-02 23:04:19 +0900

    日本語fontの導入

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -43,6 +43,29 @@
     LC_TIME = "ja_JP.UTF-8";
   };
 
+  i18n.inputMethod = {
+   enabled = "fcitx5";
+   fcitx5.addons = [pkgs.fcitx5-mozc];
+ };
+ fonts = {
+   fonts = with pkgs; [
+     noto-fonts-cjk-serif
+     noto-fonts-cjk-sans
+     noto-fonts-emoji
+     nerdfonts
+   ];
+
+   fontDir.enable = true;
+   fontconfig = {
+     defaultFonts = {
+       serif = ["Noto Serif CJK JP" "Noto Color Emoji"];
+       sansSerif = ["Noto Sans CJK JP" "Noto Color Emoji"];
+       monospace = ["JetBrainsMono Nerd Font" "Noto Color Emoji"];
+       emoji = ["Noto Color Emoji"];
+     };
+   };
+ };
+
   # Configure keymap in X11
   services.xserver.xkb = {
     layout = "us";
